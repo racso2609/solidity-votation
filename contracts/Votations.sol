5bb115d6ecbee124b0015fb92594e1b419332af9
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-02-12 18:21:44 +0000

    finish votes

--- contracts/Votations.sol
+++ contracts/Votations.sol
@@ -3,13 +3,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Votations is Ownable {
 	mapping(address => bool) user;
-	mapping(uint256 => mapping(uint32 => address)) candidates;
-	mapping(uint256 => mapping(address => uint32)) votes;
+	mapping(uint256 => mapping(uint32 => address)) public candidates;
+	mapping(uint256 => mapping(address => uint32)) public votes;
 
 	struct Votation {
 		string name;
 		uint32 rounds;
 		uint32 actualRounds;
+		uint256 startTime;
 	}
 
 	uint256 votationsQuantity;
@@ -24,11 +25,6 @@ contract Votations is Ownable {
 		return user[_user];
 	}
 
-	modifier onlyRegister() {
-		require(user[msg.sender] == true, "You are not register");
-		_;
-	}
-
 	event CreateVotation(uint256 indexed votationId, string indexed name);
 
 	function createVotations(
@@ -39,12 +35,46 @@ contract Votations is Ownable {
 		Votation memory newVotation;
 		newVotation.name = _name;
 		newVotation.rounds = _rounds;
+		newVotation.startTime = block.timestamp;
 		for (uint32 i = 0; i < _candidates.length; i++) {
-			candidates[votationsQuantity][i] = _candidates[i];
+			candidates[votationsQuantity][i + 1] = _candidates[i];
 		}
 		votationsQuantity++;
 		votations[votationsQuantity - 1] = newVotation;
 		emit CreateVotation(votationsQuantity - 1, _name);
 		return votationsQuantity - 1;
+	}
+
+	event Vote(uint256 indexed votationId, uint32 indexed candidate);
+
+	modifier onlyRegister() {
+		require(user[msg.sender] == true, "You are not register");
+		_;
+	}
+	modifier votationNotFinished(uint256 _votationId) {
+		require(
+			block.timestamp < votations[_votationId].startTime + 7 days,
+			"Votation finished"
+		);
+		_;
+	}
+
+	function makeVote(uint256 _votationId, uint32 _candidate)
+		external
+		onlyRegister
+    votationNotFinished(_votationId)
+	{
+		require(_candidate > 0, "Candidate does not exist!");
+		require(
+			candidates[_votationId][_candidate] != address(0x0),
+			"Candidate does not exist!"
+		);
+		require(
+			candidates[_votationId][_candidate] != msg.sender,
+			"You can not vote for yourselve!"
+		);
+
+		require(votes[_votationId][msg.sender] == 0, "You only can vote once!");
+		votes[_votationId][msg.sender] = _candidate;
 	}
 }
--- deploy/index.js
+++ deploy/index.js
@@ -1,24 +1,18 @@
-const CONTRACT_NAME = "Greeter";
+const CONTRACT_NAME = "Votations";
 // modify when needed
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+	const { deploy } = deployments;
+	const { deployer } = await getNamedAccounts();
 
-  // Upgradeable Proxy
-  await deploy("Greeter", {
-    from: deployer,
-    proxy: {
-      owner: deployer,
-      execute: {
-        init: {
-          methodName: "initialize",
-          args: ["First Greet"],
-        },
-      },
-    },
+	// Upgradeable Proxy
+	await deploy("Votations", {
+		from: deployer,
+		proxy: {
+			owner: deployer,
+		},
 
-    log: true,
-  });
+		log: true,
+	});
 };
 
 module.exports.tags = [CONTRACT_NAME];
